e04d6b33a452019b47c88bee05e7bf5d29ef3704
hbmarketing <hbmarketing@mail.ru>
2019-03-09 01:43:52 +0300

    some changes

---
+++ static2/scripts/header.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var burger = document.querySelector('.icon-burger');
+  var close = document.querySelector('.icon-close');
+  var head = document.querySelector('.head');
+  var nav2 = document.querySelector('.nav2');
+  var navItem = document.querySelector('.nav-item');
+
+  function nav2Animate(first, second) {
+    nav2.animate(
+      [
+        {right: first},
+        {right: second}
+      ],
+      {
+        duration: 100,
+        easing: "linear",
+        fill: "forwards",
+      }
+    );
+  }
+
+  burger.onclick = () => {
+    burger.style.display = 'none';
+    close.style.display = 'block';
+    nav2Animate('-100%', '0');
+  }
+  close.onclick = () => {
+    close.style.display = 'none';
+    burger.style.display = 'block';
+    nav2Animate('0', '-100%');
+  }
+
+  window.addEventListener('resize', () => {
+    if (getComputedStyle(navItem).display == 'block') {
+      burger.style.display = 'none';
+      close.style.display = 'none';
+      nav2.style.right = '-100%';
+    }
+    else if (getComputedStyle(navItem).display == 'none' && getComputedStyle(close).display == 'none') {
+      burger.style.display = 'block';
+    }
+  })
+})
