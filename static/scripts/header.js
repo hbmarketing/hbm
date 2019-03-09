4c8759cf25d740fc3c6f16941e6424da99ad39fa
hbmarketing <hbmarketing@mail.ru>
2019-03-09 13:40:27 +0300

    some changes

--- static/scripts/header.js
+++ static/scripts/header.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var header = document.querySelector('header');
   var burger = document.querySelector('.icon-burger');
   var close = document.querySelector('.icon-close');
   var head = document.querySelector('.head');
   var nav2 = document.querySelector('.nav2');
   var navItem = document.querySelector('.nav-item');
+
+  header.style.height = (document.documentElement.clientHeight - head.offsetHeight) + 'px';
 
   function nav2Animate(first, second) {
     nav2.animate(
@@ -31,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   window.addEventListener('resize', () => {
+    header.style.height = (document.documentElement.clientHeight - head.offsetHeight) + 'px';
+
     if (getComputedStyle(navItem).display == 'block') {
       burger.style.display = 'none';
       close.style.display = 'none';
